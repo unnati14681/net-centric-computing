6219c36f9eed66605f10e93afc5596b44704b305
Unnati Shrestha <107242890+unnati14681@users.noreply.github.com>
2024-11-19 20:45:26 +0545

    Add files via upload

---
+++ swap numbers/swap numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine($"Before Swap: a = {a}, b = {b}");
+
+            // Swapping using addition and subtraction
+            a = a + b;  
+            b = a - b;  
+            a = a - b;  
+
+            Console.WriteLine($"After Swap: a = {a}, b = {b}");
+        }
+    }
+}
